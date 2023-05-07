0f2061b752cb1f8044991276bc43f5f6ed7d49f9
yanakorobova <08mickey01@gmail.com>
2023-05-07 23:26:46 +0300

    added store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,15 @@
 import {AnyAction, combineReducers, configureStore, ThunkDispatch} from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import {appReducer} from "app/app-slice";
+import {booksReducer} from "features/ListBooks/books-slice";
+import {settingsReducer} from "features/Header/FiltersPanel/settings-slice";
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+    app: appReducer,
+    books: booksReducer,
+    settings: settingsReducer
+})
 export const store = configureStore({
     reducer: rootReducer,
     middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunk),
@@ -13,3 +20,6 @@ export type AppRootStateType = ReturnType<typeof rootReducer>
 export type AppThunkDispatch = ThunkDispatch<AppRootStateType, any, AnyAction>
 export const useAppDispatch = () => useDispatch<AppThunkDispatch>()
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
+
+//@ts-ignore
+window.store = store
