f673ffbc3c4c3bde9200c769b74b96216ac055d1
yanakorobova <08mickey01@gmail.com>
2023-05-09 22:23:35 +0300

    added Loader

---
+++ src/common/components/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from "./Loader.module.scss"
+
+export const Loader = () => {
+    return (
+        <div className={s.loader}>
+            <span className={s.loading}>...</span>
+        </div>
+
+    );
+};
+
