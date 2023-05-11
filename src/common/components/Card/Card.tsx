e12393802c505512a78fab3203904f91d2ab3cbf
yanakorobova <08mickey01@gmail.com>
2023-05-11 22:22:14 +0300

    feat: added redirect to details

--- src/common/components/Card/Card.tsx
+++ src/common/components/Card/Card.tsx
@@ -1,30 +1,36 @@
 import React from 'react';
-import {VolumeInfoType} from "common/type/type";
+import {BooksListType} from "common/type/type";
 import s from './Card.module.scss'
 import defaultImg from 'assets/images/default.jpg'
 import {useAppDispatch} from "app/store";
 import {changeCategory, changeInauthor} from "features/Header/FiltersPanel/filters-slice";
 import {Rate} from "antd";
+import {useNavigate} from "react-router-dom";
 
 type CardPropsType = {
-    info: VolumeInfoType
+    info: BooksListType
 }
 export const Card: React.FC<CardPropsType> = ({info}) => {
-    const {authors, title, categories, imageLinks, averageRating} = info
+    const {authors, title, categories, imageLinks, averageRating} = info.volumeInfo
     const dispatch = useAppDispatch()
+    const navigate = useNavigate()
     const searchBookByAuthorHandler = (author: string) => {
         dispatch(changeInauthor({inauthor: author}))
     }
     const searchBookByCategoryHandler = (category: string) => {
         dispatch(changeCategory({category}))
     }
+    const RedirectToBookHandler = () => {
+        navigate(`/book/${info.id}`)
+    }
 
     return (
         <div className={s.card}>
-            <img src={imageLinks?.thumbnail || defaultImg} alt='book img'/>
+            <img className={ s.img} src={imageLinks?.thumbnail || defaultImg} alt='book img'
+                 onClick={RedirectToBookHandler}/>
             <Rate disabled allowHalf defaultValue={averageRating} className={s.rating}/>
             <div className={s.information}>
-                <p>{title.length > 55 ? title.slice(0, 55) + '...' : title}</p>
+                <p onClick={RedirectToBookHandler}>{title.length > 55 ? title.slice(0, 55) + '...' : title}</p>
                 {<div className={s.authors}>{authors ? <>
                     <b>Authors: </b> {authors.map((a: any, i: number) => {
                     return <span key={i} onClick={() => searchBookByAuthorHandler(a)}>{i !== 0 ? `, ${a}` : a}</span>
