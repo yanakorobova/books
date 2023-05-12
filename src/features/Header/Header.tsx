6b43afe9b17866ad4ce7a8acd789d9af726602d8
yanakorobova <08mickey01@gmail.com>
2023-05-12 19:22:32 +0300

    feat: delete header on details page

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import s from './Header.module.scss'
 import background from 'assets/images/background.jpg'
 import {FiltersPanel} from "features/Header/FiltersPanel/FiltersPanel";
+import {useAppSelector} from "app/store";
+import {selectIsShow} from "app/selectors";
 
 
 export const Header = () => {
-
-    return (
-        <header className={s.header} style={{backgroundImage: `url(${background})`}}>
-            <div className={s.container}>
-                <h1>Search for Books</h1>
-                <FiltersPanel/>
-            </div>
-        </header>
+    const isShow = useAppSelector(selectIsShow)
+    return (<>
+            {isShow && <header className={s.header} style={{backgroundImage: `url(${background})`}}>
+                <div className={s.container}>
+                    <h1>Search for Books</h1>
+                    <FiltersPanel/>
+                </div>
+            </header>}
+        </>
     );
 };
 
